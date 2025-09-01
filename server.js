4def1101dcda69212adbdc0903e235461b444ba4
VikashMeena777 <136349370+VikashMeena777@users.noreply.github.com>
2025-09-01 08:14:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ dotenv.config();
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
+app.use(cors({ origin: 'https://frontend-o4v5.onrender.com', || 'https://frontend-o4v5.onrender.com' 
+             credentials: true
+}));
 app.use(express.json({ limit: '15mb' }));
 app.use(rateLimiter);
 
